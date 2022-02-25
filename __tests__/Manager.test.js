9313b50ff4b5457a279f09a9de09593d36360152
Bort <93967399+strudelAndCoffee@users.noreply.github.com>
2022-02-24 23:34:30 -0600

    manager class adds officeNumber

--- __tests__/Manager.test.js
+++ __tests__/Manager.test.js
@@ -1,7 +1,11 @@
 const Manager = require("../lib/Manager");
 
 test("checks if Manager creates an object", () => {
-    const manager = new Manager("Diana");
+    const manager = new Manager("Diana", 2, "@email.com", 42);
 
     expect(manager.name).toEqual("Diana");
+    expect(manager.id).toEqual(expect.any(Number));
+    expect(manager.email).toEqual(expect.any(String));
+    expect(manager.officeNum).toEqual(expect.any(Number));
+    expect(manager.getRole()).toEqual("Manager");
 });
