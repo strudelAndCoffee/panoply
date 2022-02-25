cfe8143b2794fb1b83aaccfa7d7bc4e4b462591c
Bort <93967399+strudelAndCoffee@users.noreply.github.com>
2022-02-24 23:19:32 -0600

    employee test checks methods

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,3 +1,14 @@
 const Employee = require("../lib/Employee");
 
-test("checks if Employee creates an object", () => {});
+test("checks if new Employee creates an object", () => {
+    const emp = new Employee("Ann", 1, "@email.com");
+
+    expect(emp.name).toEqual(expect.any(String));
+    expect(emp.id).toEqual(expect.any(Number));
+    expect(emp.email).toEqual(expect.any(String));
+
+    expect(emp.getName()).toEqual("Ann");
+    expect(emp.getId()).toEqual(1);
+    expect(emp.getEmail()).toEqual("@email.com");
+    expect(emp.getRole()).toEqual("Employee");
+});
