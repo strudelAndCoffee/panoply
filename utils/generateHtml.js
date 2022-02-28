763720b9856e0ed77aeeb681372b9122dbebf7a4
Bort <93967399+strudelAndCoffee@users.noreply.github.com>
2022-02-27 18:50:23 -0600

    added icons and teaked colors for index.html style

--- utils/generateHtml.js
+++ utils/generateHtml.js
@@ -7,25 +7,29 @@ const addSection = obj => {
     let email = obj.email;
     let other = "";
     let otherTag = "";
+    let icon = "";
 
     if (role == "Manager") {
         otherTag = "Office Number";
         other = obj.officeNumber;
+        icon = "compass";
     }
     else if (role == "Engineer") {
         otherTag = "GitHub";
         other = `<a href='https://github.com/${obj.github}' target='_blank'>${obj.github}</a>`;
+        icon = "wrench";
     }
     else if (role == "Intern") {
         otherTag = "School";
         other = obj.school;
+        icon = "pencil";
     }
 
 return `
 <div class="box column is-narrow mx-2 has-background-light">
 <div class="pb-5">
-    <h2 class="title is-2">${name}</h2>
-    <h3 class="title is-3">${role}</h3>
+    <h2 class="title is-2 has-text-link-dark">${name}</h2>
+    <h3 class="title is-4"><span class="oi oi-${icon}"></span> ${role}</h3>
 </div>
 <div class="box columns">
     <div class="column is-narrow is-flex is-flex-direction-column is-align-items-flex-end">
@@ -63,12 +67,13 @@ return `
 <meta http-equiv="X-UA-Compatible" content="ie=edge">
 <title>Panoply</title>
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.3/css/bulma.min.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/open-iconic/1.1.1/font/css/open-iconic-bootstrap.min.css">
 <link rel="stylesheet" href="./src/style.css">
 </head>
 
 <body>
 <header class="section has-background-success">
-<h1 class="title is-1 has-text-light has-text-centered">My Team</h1>
+    <h1 class="title is-1 has-text-centered has-text-weight-bold">My Team <span class="oi oi-layers has-text-warning"></span></h1>
 </header>
 
 <main class="section columns is-flex is-flex-wrap-wrap is-justify-content-center">
